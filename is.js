35031033da3dcc9ee4b2da16e8d91604cf35d9d1
karan926 <72694599+karan926@users.noreply.github.com>
2021-01-15 23:38:13 +0530

    Add files via upload

---
+++ is.js
@@ -0,0 +1,8 @@
+AOS.init({
+    duration: 900
+   });
+// var height = $(window).height();//gets height from device
+// var width = $(window).width(); //gets width from device
+
+// $(".top-container").width(width+"px");
+// $(".top-container").height(height+"px");
